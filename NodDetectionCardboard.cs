16d1e8701173f28c9b16006aac883c7096b667d7
Andre <60711778+CordeiroAndre@users.noreply.github.com>
2021-02-23 20:22:04 -0300

    Add files via upload

---
+++ NodDetectionCardboard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodDetectionCardboard : MonoBehaviour
+{
+    private Vector3 angles;
+    public float timeLimit; // Time limit to detect the head gesture
+    private bool up = false, down = false;
+
+    
+    void checkMovement()
+    {
+        if (angles.x < 330 && angles.x > 300 && !up)
+        {
+            up = true;
+        }
+        if (angles.x < 30 && angles.x > 20 && !down)
+        {
+            down = true;
+        }
+
+        if (up && down) // head nod complete condition
+        {
+            //put the method you with to call when the nod completes here.
+            resetGesture();
+
+        }
+    }
+    public void resetGesture()
+    {
+        angles = new Vector3();
+        up = false;
+        down = false;
+    }
+
+
+    void Start()
+    {
+        InvokeRepeating("resetGesture", 0f, timeLimit);
+    }
+
+
+    void Update()
+    {
+        angles = Camera.main.transform.rotation.eulerAngles;
+        checkMovement();
+    }
+}
